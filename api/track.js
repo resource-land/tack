f3d098b4b7ffb9b08d6137379736acbd9809e7b2
resource-land <info@mediaoffice-ht-bd.org>
2025-07-06 14:33:44 +0600

    Update track.js

--- api/track.js
+++ api/track.js
@@ -7,20 +7,10 @@ const supabase = createClient(
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtodmp2enNoeWhmb29va2JvYXFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA2OTIyNzIsImV4cCI6MjA2NjI2ODI3Mn0.FdOwlFP05seSbF69ErbFOyM3uO37Rul9vaLCX7bu0tg"
 );
 
-// Map types to corresponding image filenames
-const imageMap = {
-  default: "pixel.png",
-  alt: "pixel-alt.png",
-  protest: "pixel-protest.png",
-  red: "pixel-red.png",
-  green: "pixel-green.png",
-  // Add more types here as needed
-};
-
 module.exports = async (req, res) => {
   try {
     const emailId = req.query.emailId || "unknown";
-    const type = req.query.type || "default";
+    const type = req.query.type || "default"; // NEW: Add a type param to decide which image to serve
 
     const logEntry = {
       emailId,
@@ -38,10 +28,9 @@ module.exports = async (req, res) => {
       console.log("Logged entry:", logEntry);
     }
 
-    // Lookup the image file based on type
-    const imageFile = imageMap[type] || imageMap["default"];
+    // Serve different images based on 'type'
+    const imageFile = type === "alt" ? "pixel-alt.png" : "pixel.png";
     const imgPath = path.join(__dirname, "..", imageFile);
-    console.log(imageFile," Image path is: ",imgPath)
 
     if (!fs.existsSync(imgPath)) {
       return res.status(404).send("Image not found");
@@ -50,7 +39,6 @@ module.exports = async (req, res) => {
     const img = fs.readFileSync(imgPath);
     res.setHeader("Content-Type", "image/png");
     return res.status(200).send(img);
-    
   } catch (err) {
     console.error("Handler Error:", err);
     return res.status(500).send("Internal Server Error");
