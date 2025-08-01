503d0f20fb5eef6f547b0510b32866c051479c12
resource-land <info@mediaoffice-ht>
2025-08-01 16:12:12 +0600

    update status

--- api/index.js
+++ api/index.js
@@ -70,7 +70,7 @@ module.exports = async (req, res) => {
         },
         usaTarif: {
             displayName: "USA tarif Campaign",
-            totalSent: 1005
+            totalSent: 2500
         },
     };
 
