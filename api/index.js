5d07155392f0f7639a4ee9acde22bd8c3121cdc5
resource-land <info@mediaoffice-ht>
2025-09-12 17:22:15 +0600

    total updated

--- api/index.js
+++ api/index.js
@@ -81,7 +81,7 @@ module.exports = async (req, res) => {
         },
         inviteCard:{
             displayName: "Bangladesh 2.0 Invite Card",
-            totalSent: 3500,
+            totalSent: 3765,
             baseOpens: 0 // You can set it to 0 if none
         },
     };
